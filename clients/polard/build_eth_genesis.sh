54ec8cf404d81d6605f83d029e16bf36d820b7b7
arch@berachain.com <arch@berachain.com>
2023-05-18 14:40:25 -0400

    bet

--- clients/polard/build_eth_genesis.sh
+++ clients/polard/build_eth_genesis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Extract eth_genesis from polaris_genesis.json
-eth_genesis=$(cat polaris_genesis.json | jq -r '.app_state.evm.params.eth_genesis')
+# Extract eth_genesis from config/genesis.json
+eth_genesis=$(cat config/genesis.json | jq -r '.app_state.evm.params.eth_genesis')
 
 # Remove escape characters and format the JSON
 formatted_eth_genesis=$(echo -n "$eth_genesis" | sed 's/\\//g' | jq '.')
@@ -9,4 +9,4 @@ formatted_eth_genesis=$(echo -n "$eth_genesis" | sed 's/\\//g' | jq '.')
 # Create a new file called genesis.json with formatted_eth_genesis
 echo "$formatted_eth_genesis" > genesis.json
 
-echo "Successfully created genesis.json"
+echo "Successfully created ethereum genesis.json"
